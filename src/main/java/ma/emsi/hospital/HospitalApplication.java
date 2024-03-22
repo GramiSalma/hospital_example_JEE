8f51547932e1b361197fcc6cc10e361bb87393b8
GRAMI SALMA <salmagrami0@gmail.com>
2024-03-22 17:05:08 +0000

    Creation des repositories +quelques patients

--- src/main/java/ma/emsi/hospital/HospitalApplication.java
+++ src/main/java/ma/emsi/hospital/HospitalApplication.java
@@ -1,13 +1,33 @@
 package ma.emsi.hospital;
 
+import ma.emsi.hospital.entities.Patient;
+import ma.emsi.hospital.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-public class HospitalApplication {
+public class HospitalApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
+@Bean
+	CommandLineRunner start(PatientRepository patientRepository){
+		return args->{
+			Stream.of("Mohammed","Hassan","Najat")
+					.forEach(name->{
+						Patient patient=new Patient();
+						patient.setDateNaissnace(new Date());
+						patient.setNom(name);
+						patient.setMalade(false);
+						patientRepository.save(patient);
+					});
 
+		};
+	}
 }
---
+++ src/main/java/ma/emsi/hospital/repositories/MedecinRepository.java
@@ -0,0 +1,7 @@
+package ma.emsi.hospital.repositories;
+
+import ma.emsi.hospital.entities.Medecin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedecinRepository extends JpaRepository<Medecin,Long> {
+}

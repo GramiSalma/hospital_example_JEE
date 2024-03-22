abdda987f34d29d3e3c2a5b9cebfd6693acede59
GRAMI SALMA <salmagrami0@gmail.com>
2024-03-22 17:38:18 +0000

    ajout de quelque consultations et rendez-vous

--- src/main/java/ma/emsi/hospital/HospitalApplication.java
+++ src/main/java/ma/emsi/hospital/HospitalApplication.java
@@ -1,9 +1,10 @@
 package ma.emsi.hospital;
 
-import ma.emsi.hospital.entities.Medecin;
-import ma.emsi.hospital.entities.Patient;
+import ma.emsi.hospital.entities.*;
+import ma.emsi.hospital.repositories.ConsultationRepository;
 import ma.emsi.hospital.repositories.MedecinRepository;
 import ma.emsi.hospital.repositories.PatientRepository;
+import ma.emsi.hospital.repositories.RendezVousRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +21,9 @@ public class HospitalApplication  {
 	}
 @Bean
 	CommandLineRunner start(PatientRepository patientRepository,
-							MedecinRepository medecinRepository){
+							MedecinRepository medecinRepository,
+							RendezVousRepository rendezVousRepository,
+							ConsultationRepository consultationRepository){
 		return args->{
 			Stream.of("Mohammed","Hassan","Najat")
 					.forEach(name->{
@@ -35,9 +38,28 @@ public class HospitalApplication  {
 						Medecin medecin =new Medecin ();
 						medecin.setNom(name);
 						medecin.setSpecialite(Math.random()>0.5?"Cardio":"Dentiset");
-
+						medecin.setEmail(name+"@gmail.com");
 						medecinRepository.save(medecin);
 					});
+            Patient patient=patientRepository.findById(1L).orElse(null);
+			Patient patient1=patientRepository.findByNom("Hassan");
+			Medecin medecin=medecinRepository.findByNom("Medecin1");
+
+
+			RendezVous rendezVous=new RendezVous();
+			rendezVous.setDate(new Date());
+			rendezVous.setStatus(StatusRDV.PENDING);
+			rendezVous.setMedecin(medecin);
+			rendezVous.setPatient(patient);
+			rendezVousRepository.save(rendezVous);
+
+			RendezVous rendezVous1=rendezVousRepository.findById(1L).orElse(null);
+			Consultation consultation=new Consultation();
+			consultation.setDateConsultation(new Date());
+			consultation.setRendezVous(rendezVous);
+			consultation.setRapport("Rapport de la cnsultation ....");
+			consultationRepository.save(consultation);
+
 
 		};
 	}
--- src/main/java/ma/emsi/hospital/repositories/PatientRepository.java
+++ src/main/java/ma/emsi/hospital/repositories/PatientRepository.java
@@ -4,4 +4,6 @@ import ma.emsi.hospital.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Patient findByNom(String name);
+
 }
